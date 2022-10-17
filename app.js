442d8e1d8e5b6f79d8de3ecebf348be5a6537673
Francis-Frimpong <brakay1136@gmail.com>
2022-10-17 17:53:34 +0000

    inproved calculateBmi function and html label

--- app.js
+++ app.js
@@ -7,13 +7,27 @@ const  displayResult= document.querySelector(".display-result");
 //adding the click event to the submit button
 submitBtn.addEventListener('click',(e)=>{
     e.preventDefault("entryField")
+    calculateBmi()
     clearfield()
     
 })
 
 //funntion for BMI calculation
 let calculateBmi = ()=>{
+    const weight = +weightValue.value ;
+    const height = +heightValue.value ;
 
+
+    //Converting the height in centimeters to meters
+    let convertHeight = height * 0.01;
+
+    //square the convertHeight
+    let squeredHeight = convertHeight**2;
+
+    //calculate the body mass index(BMI)
+    let calculateBmi = weight / squeredHeight;
+    
+    displayResult.textContent = +calculateBmi.toFixed(2);
 };
 
 //funtion for clearing the formfield when the submit button has been clicked
